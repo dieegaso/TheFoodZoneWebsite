8231ef354e917899a015450be5b1915c3b3f16f4
Diego <dieegaso4@gmail.com>
2025-05-29 18:10:27 -0500

    Update eventsP.js
    changed paths

--- includes/eventsP.js
+++ includes/eventsP.js
@@ -1,7 +1,7 @@
 export const eventsP = [
 {
-    name: "El Toro Loco",
-    image: "../assets/events/toroLoco.jpg",
+    name: "El Aracloso",
+    image: "assets/events/toroLoco.jpg",
     url: "https://unsplash.com/photos/a-bull-with-large-horns-standing-in-a-field-LtbnpeWBDF8?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
     description: "Ven a subirte al toro de los tacos regios el coquis. El toro viene de tamaulipas, donde la vida no vale nada",
     date: ["June 4, 2025"],
@@ -11,7 +11,7 @@ export const eventsP = [
 },
 {
     name: "Pokemon Day",
-    image: "../assets/events/event2.jpg",
+    image: "assets/events/event2.jpg",
     url: "https://unsplash.com/photos/yellow-and-white-duck-plush-toy-on-green-grass-during-daytime-DypO_XgAE4Y",
     description: "Some nerds are overpaying for some mid-looking cards",
     date: ["June 1, 2025"],
@@ -21,7 +21,7 @@ export const eventsP = [
 },
 {
     name: "Atropellados",
-    image: "../assets/events/traffic.jpg",
+    image: "assets/events/traffic.jpg",
     url:"https://unsplash.com/photos/a-group-of-cars-driving-down-a-street-next-to-each-other-toaX1dyZjcA?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
     description: "We are making traffict at FM-1314, everyone that comes into the foodzone is supposed to form a line inside the food park and never exit their vehicles",
     date: ["May 30, 2025"],
@@ -31,7 +31,7 @@ export const eventsP = [
 },
 {
     name: "Loteria Night",
-    image: "../assets/events/loteria.jpg",
+    image: "assets/events/loteria.jpg",
     url: "https://unsplash.com/photos/blue-white-and-red-playing-cards-clelay10tfg",
     description: "Come play loteria every thursday from 7:00pm to 8:00pm and win prizes",
     date: ["May 1, 2025", "Jun 30, 2025"],
@@ -41,7 +41,7 @@ export const eventsP = [
 },
 {
     name: "Ruido at the FZ",
-    image: "../assets/events/radio.jpg",
+    image: "assets/events/radio.jpg",
     url: "https://unsplash.com/photos/selective-focus-photo-of-dj-mixer-73o_FzZ5x-w",
     description: "El Dj del foodzone y Raicez bar se implican en una batalla por ver quien pone la musica mas alta!",
     date: ["May 5, 2025", "Aug 24, 2025"],
