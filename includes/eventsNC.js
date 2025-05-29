85d6b2a090362ada9fa8d9ab993f3a7d977deffb
Diego <dieegaso4@gmail.com>
2025-05-29 18:06:23 -0500

    Update eventsNC.js
    Changed Paths

--- includes/eventsNC.js
+++ includes/eventsNC.js
@@ -1,7 +1,7 @@
 export const eventsNC = [
 {
     name: "Field Day",
-    image: "../assets/events/event3.jpg",
+    image: ".assets/events/event3.jpg",
     url: "https://unsplash.com/photos/people-walking-on-park-near-high-rise-buildings-during-daytime-AUmq5VsgL3g",
     description: "Field day at the foodzone where we are going to be dropping food out of people's hands with our games",
     date: ["May 30, 2025"],
@@ -11,7 +11,7 @@ export const eventsNC = [
 },
 {
     name: "Pokemon Day",
-    image: "../assets/events/event2.jpg",
+    image: "assets/events/event2.jpg",
     url: "https://unsplash.com/photos/yellow-and-white-duck-plush-toy-on-green-grass-during-daytime-DypO_XgAE4Y",
     description: "Some nerds are overpaying for some mid-looking cards",
     date: ["May 28, 2025"],
@@ -21,7 +21,7 @@ export const eventsNC = [
 },
 {
     name: "Hockey Day",
-    image: "../assets/events/event1.jpg",
+    image: "assets/events/event1.jpg",
     url:"https://unsplash.com/photos/man-wearing-green-and-white-ice-hockey-jersey-while-playing-doivHPaG-Vw",
     description: "We are playing Hockey in TEXAS, I dont know but I would come to that If we were you",
     date: ["May 29, 2025"],
@@ -31,7 +31,7 @@ export const eventsNC = [
 },
 {
     name: "Lol Day",
-    image: "../assets/events/event4.jpg",
+    image: "assets/events/event4.jpg",
     url: "https://unsplash.com/photos/long-coated-brown-animal-MCauAnBJeig",
     description: "As soon as you enter the foodzone, you need to start laughing non-stop until you leave. Or you die :)",
     date: ["May 31, 2025"],
@@ -41,7 +41,7 @@ export const eventsNC = [
 },
 {
     name: "Loteria Night",
-    image: "../assets/events/loteria.jpg",
+    image: "assets/events/loteria.jpg",
     url: "https://unsplash.com/photos/blue-white-and-red-playing-cards-clelay10tfg",
     description: "Come play loteria every thursday from 7:00pm to 8:00pm and win prizes",
     date: ["May 1, 2025", "Jun 30, 2025"],
@@ -51,7 +51,7 @@ export const eventsNC = [
 },
 {
     name: "Live Music DJ",
-    image: "../assets/events/dj.jpg",
+    image: "assets/events/dj.jpg",
     url: "https://unsplash.com/photos/man-controlling-dj-controller-DGKJzOmjyS4?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
     description: "We pay an overated guy $1,000 to come and play his spotify playlist for y'all. Youre welcome!",
     date: ["May 5, 2025", "Aug 24, 2025"],
